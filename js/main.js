b9515ce0ef7c08ccee8703ebd602c1f9a310f7a6
david <sixfngers@yahoo.com>
2014-08-16 14:06:31 -0700

    merge with basic layout2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
- 
-  $("#owl-demo").owlCarousel({
-    items : 4,
-    lazyLoad : true,
-    navigation : true
-  }); 
+    
 
-	$('.project-tile').hover( 
-	    function(){
-	    	$(this).children('.lower').children('.project-roles').animate({marginBottom:'0px'},'fast')
-	    	$(this).children('.upper').children('.tile-thumb').animate({height:'120%'},'slow')
-	    },
-	    function(){
-	    	$(this).children('.lower').children('.project-roles').animate({marginBottom:'8px'},'fast')
-	    	$(this).children('.upper').children('.tile-thumb').animate({height:'100%'},'slow')
-    });
+
+
+
+
+
+  $('.owl-carousel').owlCarousel({
+      items:1,
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:5000,
+      controls:true
+  });
+
+  $('.project-tile').hover( 
+      function(){
+          $(this).children('.lower').children('.project-roles').animate({marginBottom:'0px'},'fast')
+          $(this).children('.upper').children('.tile-thumb').animate({height:'120%'},'slow')
+          TweenMax.to($(this), .5, {boxShadow:"2px 2px 2px rgba(0, 0, 0, .2);"});
+      },
+      function(){
+          $(this).children('.lower').children('.project-roles').animate({marginBottom:'8px'},'fast')
+          $(this).children('.upper').children('.tile-thumb').animate({height:'100%'},'slow')
+          TweenMax.to($(this), .5, {boxShadow:"2px 2px 2px rgba(0, 0, 0, 0);"});
+  });
 
 });

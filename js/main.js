7ef1dc32c6e038dd0634c98f5c2cb7ec7c6de56b
david <sixfngers@yahoo.com>
2014-08-16 13:58:59 -0700

    layout updates

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
 
   $('.owl-carousel').owlCarousel({
       items:1,
+      loop:true,
       autoplay:true,
       autoplayTimeout:5000,
-      autoplayHoverPause:true,
       controls:true
   });
 
